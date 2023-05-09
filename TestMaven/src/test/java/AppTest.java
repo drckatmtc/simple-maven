c945787d093a0bb6acc202ec13fbf70a4f5b7d52
drckatmtc <104545349+drckatmtc@users.noreply.github.com>
2023-05-09 20:36:11 +0800

    Add files via upload

---
+++ TestMaven/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	
+	@Test
+	public void sampleAssert() {
+		assertTrue(true);
+	}
+	
+
+}
